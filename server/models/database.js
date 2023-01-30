16846fcecc38ff9022ba705bce6fcf78888781b4
Ajay Panchal <ajaypanchal7778@gmail.com>
2023-01-31 03:51:40 +0530

    second

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://ajay:ajay123@cluster0.0bjypqx.mongodb.net/Recipes?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+MONGODB_URI=process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
